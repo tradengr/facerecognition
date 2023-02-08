0d263f5d1a2f5fdb1e7198d02cc384f016467c40
Nicolo <nicolo.valbuena@protonmail.com>
2023-02-08 11:53:45 +0800

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -37,30 +37,18 @@ class App extends Component {
       joined: data.joined
     }})
   }
-
   onLinkChange = (event) => {
     this.setState({ imageUrl: event.target.value });
   }
-
   onDetect = () => {
     this.setState({ imageUrlDetected: this.state.imageUrl })
-    //////////////////////////////////////////////////////////////////////////////////////////
-    // In this section, we set the user authentication, app ID, model details, and the URL
-    // of the image we want as an input. Change these strings to run your own example.
-    /////////////////////////////////////////////////////////////////////////////////////////
 
     const USER_ID = 'tradengr';
-    // Your PAT (Personal Access Token) can be found in the portal under Authentification
     const PAT = '0e0879a68c534882a070f31041143d1f';
     const APP_ID = 'my-first-application';
-    // Change these to whatever model and image URL you want to use
     const MODEL_ID = 'face-detection';
     const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';    
     const IMAGE_URL = this.state.imageUrl;
-
-    ///////////////////////////////////////////////////////////////////////////////////
-    // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
-    ///////////////////////////////////////////////////////////////////////////////////
 
     const raw = JSON.stringify({
         "user_app_id": {
@@ -86,10 +74,6 @@ class App extends Component {
         },
         body: raw
     };
-
-    // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
-    // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
-    // this will default to the latest version_id
 
     fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
         .then(response => response.json())
@@ -138,31 +122,31 @@ class App extends Component {
   }
 
   render() {
-    const { route, box, imageUrlDetected } = this.state;
+    const { route, box, imageUrlDetected, user } = this.state;
+    const { onRouteChange, loadUser, onLinkChange, onDetect } = this;
     return (
-      <div>
+      <>
         <ParticlesBg type='cobweb' bg={true} color='999999' num={200} />
-        <Navigation onRouteChange={this.onRouteChange} route={route} />
+        <Navigation onRouteChange={onRouteChange} route={route} />
         { route === 'signin'
             ? <>
                 <Logo />
-                <SignIn onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
+                <SignIn onRouteChange={onRouteChange} loadUser={loadUser} />
               </>
             : route === 'home'
             ? <>
                 <Logo />
-                <StatusBar userName={this.state.user.name} imageDetected={this.state.user.imageDetected} />
-                <LinkForm onLinkChange={this.onLinkChange} onDetect={this.onDetect} />
+                <StatusBar userName={user.name} imageDetected={user.imageDetected} />
+                <LinkForm onLinkChange={onLinkChange} onDetect={onDetect} />
                 <FaceRecognition imageUrlDetected={imageUrlDetected} box={box} />
               </>
             : route === 'signup'
-            ? <SignUp onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
+            ? <SignUp onRouteChange={onRouteChange} loadUser={loadUser} />
             : <Logo />
         }
-      </div>
+      </>
     );
   }
 }
 
 export default App;
-
